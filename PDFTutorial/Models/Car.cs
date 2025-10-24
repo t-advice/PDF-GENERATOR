1d16804158fded9c4476cc785fab45aeacd9cf6b
Tashwill Advice <tashwilladvice02@gmail.com>
2025-10-24 10:07:30 +0200

    car object model added .

--- PDFTutorial/Models/Car.cs
+++ PDFTutorial/Models/Car.cs
@@ -9,5 +9,12 @@ namespace PDFTutorial.Models
     public class Car
     {
         public string VIN { get; set; } = "XYZ1234567890ABC"; // Default VIN for a car.
+        public string Make { get; set; } = "Ford";
+        public string Model { get; set; } = "Mustang GT";
+        public int Year { get; set; } = 2024;
+        public string Color { get; set; } = "Race Red";
+        public double EngineSizeL { get; set; } = 5.0;
+        public int Horsepower { get; set; } = 486;
+        public decimal Price { get; set; } = 55995.00m;
     }
 }
